654d974262a9e8781594b0ca618cf92cf6220cf8
ndsh <me@julian-h.de>
2021-05-28 16:17:47 +0200

    clemens update

--- Kurzzeitprojekt/Clemens/BlynkReadData/BlynkReadData.ino
+++ Kurzzeitprojekt/Clemens/BlynkReadData/BlynkReadData.ino
@@ -3,21 +3,15 @@
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
-char auth[] = "auth_tiken";
+char auth[] = "auth_token";
 
-char ssid[] = "ssid";
+char ssid[] = "id";
 char pass[] = "pw";
 
 float value = 0;
 
 BLYNK_WRITE(V1) { // V1 = Virtueller Pin 1
-  int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
-  // You can also use:
-  // String i = param.asStr();
-  // double d = param.asDouble();
-  Serial.print("V1 Slider value is: ");
-  Serial.println(pinValue);
-  value = pinValue;
+  value = param.asInt();
 }
 
 void setup() {
